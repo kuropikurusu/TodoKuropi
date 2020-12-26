dd3d6d53e9ae4efdb462f6eb01d31a85fd60ff57
kuropikurusu <egdeh.nob.yupikurokai1019@gmail.com>
2020-12-27 05:23:55 +0900

    second commit

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//classの継承を追加
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    //UITableView、numberOfRowsInSectionの追加(表示するcell数を決める)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //戻り値の設定(表示するcell数)
+        return TodoKobetsunonakami.count
+    }
+    
+    //UITableView、cellForRowAtの追加(表示するcellの中身を決める)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //変数を作る
+        let TodoCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        //変数の中身を作る
+        TodoCell.textLabel!.text = TodoKobetsunonakami[indexPath.row]
+        //戻り値の設定（表示する中身)
+        return TodoCell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
